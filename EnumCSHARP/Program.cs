ce4b9c3a943347ef3fecebe4afb40c47df1db34f
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-21 12:47:24 -0300

    First step Finalized

--- EnumCSHARP/Program.cs
+++ EnumCSHARP/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EnumCSHARP.Entities;
+using EnumCSHARP.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,23 @@ namespace EnumCSHARP
     {
         static void Main(string[] args)
         {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Processing
+
+            };
+            Console.WriteLine(order);
+            string txt = order.Status.ToString();
+            int num = (int) order.Status;
+            OrderStatus os;
+            Enum.TryParse<OrderStatus>("Delivered",out os);
+            Console.WriteLine(num);
+            Console.WriteLine(txt);
+            Console.ReadKey();
+
+
         }
     }
 }
